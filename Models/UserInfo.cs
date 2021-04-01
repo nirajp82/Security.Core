ce7f606dc94fc4e09b99e29ceddc80b61f1614c1
Niraj Patel <nirajp82@yahoo.com>
2021-04-01 18:37:50 -0400

    Add project files.

---
+++ Models/UserInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Security.Core
+{
+    public class UserInfo
+    {
+        public string UserName { get; set; }
+        public string UserId { get; set; }
+        public Dictionary<string, string> UserClaims { get; set; }
+    }
+}
